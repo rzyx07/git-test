f1e4128c7a09d3bd95a0829205e095e87d9b3c0e
rzyx <rzyx@tyx.com>
2022-09-07 19:35:29 +0800

    hot-fix commit

--- src/main/java/org/rzyx/Hello.java
+++ src/main/java/org/rzyx/Hello.java
@@ -6,5 +6,6 @@ public class Hello {
         System.out.println("Hello Git!");
         System.out.println("Hello Git!");
         System.out.println("Hello Git!");
+        System.out.println("Hello Git!");
     }
 }
